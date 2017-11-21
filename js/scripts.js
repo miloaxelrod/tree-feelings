64972612c66ccc19b811b3a0d7d17069355936e0
miloaxelrod <miloaxelrod@gmail.com>
2017-11-20 22:55:09 -0500

    adding all the stuff so far

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+
+
+ $(document).ready(function() {
+
+/* MVP */
+
+/*
+  > create a little form to enter a couple forms about a trees
+  > save it to firebase
+
+  > manually add five tree ids
+  > call api by random id (of those five)
+  > display name and image for that tree
+  > add a feeling
+  > display the feeling
+*/
+
+
+/* search box */
+
+/*
+  > upon searchbox-btn click, grab searchbox-text val and put it in a variable
+  > apply that variable to the database in a search for keyword
+  > store those objects in an array
+  > go to search results page
+  > in a loop, display thumbnails on the page for each objects in the array
+*/
+
+/* add trees to database */
+
+/*
+  > takes search val and stores it in a variable inside local scope
+  > clears searchbox
+
+
+  > creates a new .html page whos name is the name of the tree? OH how about the object id number from firebase, that has no spaces
+*/
+
+
+
+/* browse trees */
+
+/*
+  > make a loop that
+  > in local scope,retrieves name and image url from firebase and store in a variable
+  > insert those variables into the htlm for the tree thumbnail
+  > append that onto #browse-trees
+*/
+
+
+/* add a feeling */
+
+$('#add-entry').on("click", function() {
+  $('#submit-feeling').css('display', 'block');
+});
+
+/*
+  > on submit, get values from the form and store it in local variabels
+  > clear fields
+  > send the values to the database
+  > retrieve those values, put them into the entry-container frame
+  > append it to #entries
+*/
+
+
+
+});
