bd12713908ce277b5f2acf12d3f6e1981e7162dd
miloaxelrod <miloaxelrod@gmail.com>
2017-11-21 23:28:05 -0500

    ajax call test 2

--- js/scripts.js
+++ js/scripts.js
@@ -45,11 +45,14 @@ $('#submit-tree').submit(function(event) {
   });
 });
 
+console.log('test1');
 
 var root = 'http://eol.org/api/pages/1.0.json?batch=false&id=';
 var searchSettings = '&maps_page=1&texts_per_page=2&texts_page=1&subjects=overview&licenses=all&details=true&common_names=true&synonyms=true&references=true&taxonomy=true&vetted=0&cache_ttl=&language=en'
 
 var searchTreeId = 1061748;
+
+console.log('test2');
 
 $.ajax({
   url: root + searchTreeId + searchSettings,
@@ -57,9 +60,10 @@ $.ajax({
 }).then(function(data) {
   var treeInfo = data.results;
   console.log('data');
+  console.log('test3');
 })
 
-
+console.log('test4');
 
 /* search box */
 
